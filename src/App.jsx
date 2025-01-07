d814a3c5571d7aebf026efb12d6b8ab3e48182eb
PauloAlves8039 <pj38alves@gmail.com>
2025-01-06 21:51:48 -0300

    adiciona projeto no repositório

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Sistema de Chamados</h1>
+    </div>
+  )
+}
