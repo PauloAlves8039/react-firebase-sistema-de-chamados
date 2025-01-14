1ae40dfec25865b563499e39dc1da0e4faa8114b
PauloAlves8039 <pj38alves@gmail.com>
2025-01-14 20:12:56 -0300

    feat: direciona cadastro para dashboard

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -3,11 +3,15 @@ import { auth, db } from "../services/firebaseConnection";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
+import { useNavigate } from "react-router-dom";
+
 export const AuthContext = createContext({});
 
 export default function AuthProvider({ children }){
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+
+  const navigate = useNavigate();
 
   function signIn(email, password){
     console.log(email)
@@ -34,13 +38,19 @@ export default function AuthProvider({ children }){
               avatarUrl: null
             };
             setUser(data);
+            storageUser(data);
             setLoadingAuth(false);
+            navigate("/dashboard");
           })
       })
       .catch((error) => {
         console.log(error);
         setLoadingAuth(false);
       });
+  }
+
+  function storageUser(data){
+    localStorage.setItem("@ticketsPRO", JSON.stringify(data))
   }
 
   return(
