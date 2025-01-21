1d03e0e8805fdbfc312e4b89e001a7e945ac4016
PauloAlves8039 <pj38alves@gmail.com>
2025-01-21 20:09:17 -0300

    feat: adiciona elementos para o perfil

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -110,7 +110,9 @@ export default function AuthProvider({ children }){
         signUp,
         logout,
         loadingAuth,
-        loading
+        loading,
+        storageUser,
+        setUser
       }}
     >
       {children}
