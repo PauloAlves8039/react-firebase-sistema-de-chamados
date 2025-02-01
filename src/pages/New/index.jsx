399b66e72d3148caf6b4c8fcd50354e894762363
PauloAlves8039 <pj38alves@gmail.com>
2025-02-01 11:52:54 -0300

    feat: aplica função para editar chamado

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -6,7 +6,9 @@ import { FiPlusCircle } from "react-icons/fi";
 
 import {AuthContext} from "../../contexts/auth";
 import { db } from "../../services/firebaseConnection";
-import {collection, getDocs, getDoc, doc, addDoc} from "firebase/firestore";
+import {collection, getDocs, getDoc, doc, addDoc, updateDoc} from "firebase/firestore";
+
+import { useParams, useNavigate } from "react-router-dom";
 
 import { toast } from "react-toastify";
 
@@ -16,6 +18,8 @@ const listRef = collection(db, "customers");
 
 export default function New() {
     const { user } = useContext(AuthContext);
+    const { id } = useParams();
+    const navigate = useNavigate();
 
     const [customers, setCustomers] = useState([]);
     const [loadCustomer, setLoadCustomer] = useState(true);
@@ -24,6 +28,7 @@ export default function New() {
     const [complemento, setComplemento] = useState("");
     const [assunto, setAssunto] = useState("Suporte");
     const [status, setStatus] = useState("Aberto");
+    const [idCustomer, setIdCustomer] = useState(false);
 
     useEffect(() => {
         async function loadCustomers(){
@@ -47,6 +52,10 @@ export default function New() {
     
             setCustomers(lista);
             setLoadCustomer(false);
+
+            if (id) {
+                loadId(lista);
+            }
     
           })
           .catch((error) => {
@@ -57,7 +66,26 @@ export default function New() {
         }
     
         loadCustomers();    
-      }, []);
+    }, [id]);
+
+    async function loadId(lista) {
+        const docRef = doc(db, "chamados", id);
+
+        await getDoc(docRef)
+            .then((snapshot) => {
+                setAssunto(snapshot.data().assunto);
+                setStatus(snapshot.data().status);
+                setComplemento(snapshot.data().complemento);
+
+                let index = lista.findIndex(item => item.id === snapshot.data().clienteId);
+                setCustomerSelected(index);
+                setIdCustomer(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                setIdCustomer(false);
+            });
+    }
 
 
     function handleOptionChange(e) {
@@ -75,6 +103,31 @@ export default function New() {
 
     async function handleRegister(e) {
         e.preventDefault();
+
+        if(idCustomer) {
+            const docRef = doc(db, "chamados", id);
+
+            await updateDoc(docRef, {
+                cliente: customers[customerSelected].nomeFantasia,
+                clienteId: customers[customerSelected].id,
+                assunto: assunto,
+                complemento: complemento,
+                status: status,
+                userId: user.uid,
+            })
+            .then(() => {
+                toast.info("Chamado atualizado com sucesso!");
+                setCustomerSelected(0);
+                setComplemento("");
+                navigate("/dashboard");
+            })
+            .catch((error) => {
+                toast.error("Ops erro ao atualizar esse chamado!");
+                console.log(error);
+            });
+
+            return;
+        }
 
         await addDoc(collection(db, "chamados"), {
             created: new Date(),
@@ -101,7 +154,7 @@ export default function New() {
             <Header />
 
             <div className="content">
-                <Title name="Novo chamado">
+                <Title name={id ? "Editando Chamado" : "Novo Chamado"}>
                     <FiPlusCircle size={25} />
                 </Title>
 
